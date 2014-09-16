1d416f361ebddb5790a2da38f2a97a2cc9aec394
Nikolas <nikolas@MacBook-Nikolas-8.local>
2014-09-16 21:52:46 +0300

    Initial Commit

---
+++ Button/BTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BTAppDelegate.h
+//  Button
+//
+//  Created by Nikolas on 16.09.14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Button/BTViewController.h
@@ -0,0 +1,13 @@
+//
+//  BTViewController.h
+//  Button
+//
+//  Created by Nikolas on 16.09.14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BTViewController : UIViewController
+
+@end
